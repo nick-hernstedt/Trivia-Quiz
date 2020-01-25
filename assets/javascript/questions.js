ce69983c0cc5aba67bc4f5da2ca591124e87d442
copiousamounts <nick.hernstedt@gmail.com>
2020-01-24 16:29:26 -0800

    tried using questions.js with no luck

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,14 +1,40 @@
-var questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    ///etc.
-  ];
+//  create our questions
+let questions = [
+  {
+      question : "What species of reptile is this",
+      imgSrc : "./assets/images/coral.jpg",
+      choiceA : "Nerodia rhombifer",
+      choiceB : "Micrurus fulvius",
+      choiceC : "Ophiophagus hannah",
+      correct : "B"
+  } ,   {
+     question : "What species of bird is this",
+     imgSrc : "./assets/images/harpy.jpg",
+     choiceA : "Falco peregrinus",
+     choiceB : "Haliaeetus leucocephalus",
+     choiceC : "Harpia harpyja",
+     correct : "C"
+ } ,  {
+     question : "What species of amphibian is this",
+     imgSrc : "./assets/images/sonoran.jpg",
+     choiceA : "Megophrys nasuta",
+     choiceB : "Incilius alvarius",
+     choiceC : "Dendrobates tinctorius",
+     correct : "B"
+ } , {
+     question : "What species of mammal is this",
+     imgSrc : "./assets/images/opossum.jpg",
+     choiceA : "Didelphis virginiana",
+     choiceB : "Procyon lotor",
+     choiceC : "Pseudocheirus peregrinus",
+     correct : "A"
+ } , {
+     question : "What species of fish is this?",
+     imgSrc : "./assets/images/parrotfish.jpg",
+     choiceA : "Cetoscarus ocellatus",
+     choiceB : "Chlorurus michrohinos",
+     choiceC : "Chlorurus sordidus",
+     correct : "A"
+ } ,
+]
   
