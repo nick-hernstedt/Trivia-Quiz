534b1d1f9bcc9fb9a23dddaf38a5d9c979323bf4
copiousamounts <nick.hernstedt@gmail.com>
2020-01-24 16:37:05 -0800

    got questions working from seperate js file

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,5 @@
 // select all elements
+
 const start = document.getElementById("start");
 const quiz = document.getElementById("quiz");
 const question = document.getElementById("question");
@@ -12,44 +13,44 @@ const progress = document.getElementById("progress");
 const scoreDiv = document.getElementById("score");
 
 //  create our questions
- let questions = [
-     {
-         question : "What species of reptile is this",
-         imgSrc : "./assets/images/coral.jpg",
-         choiceA : "Nerodia rhombifer",
-         choiceB : "Micrurus fulvius",
-         choiceC : "Ophiophagus hannah",
-         correct : "B"
-     } ,   {
-        question : "What species of bird is this",
-        imgSrc : "./assets/images/harpy.jpg",
-        choiceA : "Falco peregrinus",
-        choiceB : "Haliaeetus leucocephalus",
-        choiceC : "Harpia harpyja",
-        correct : "C"
-    } ,  {
-        question : "What species of amphibian is this",
-        imgSrc : "./assets/images/sonoran.jpg",
-        choiceA : "Megophrys nasuta",
-        choiceB : "Incilius alvarius",
-        choiceC : "Dendrobates tinctorius",
-        correct : "B"
-    } , {
-        question : "What species of mammal is this",
-        imgSrc : "./assets/images/opossum.jpg",
-        choiceA : "Didelphis virginiana",
-        choiceB : "Procyon lotor",
-        choiceC : "Pseudocheirus peregrinus",
-        correct : "A"
-    } , {
-        question : "What species of fish is this?",
-        imgSrc : "./assets/images/parrotfish.jpg",
-        choiceA : "Cetoscarus ocellatus",
-        choiceB : "Chlorurus michrohinos",
-        choiceC : "Chlorurus sordidus",
-        correct : "A"
-    } ,
- ]
+//  let questions = [
+//      {
+//          question : "What species of reptile is this",
+//          imgSrc : "./assets/images/coral.jpg",
+//          choiceA : "Nerodia rhombifer",
+//          choiceB : "Micrurus fulvius",
+//          choiceC : "Ophiophagus hannah",
+//          correct : "B"
+//      } ,   {
+//         question : "What species of bird is this",
+//         imgSrc : "./assets/images/harpy.jpg",
+//         choiceA : "Falco peregrinus",
+//         choiceB : "Haliaeetus leucocephalus",
+//         choiceC : "Harpia harpyja",
+//         correct : "C"
+//     } ,  {
+//         question : "What species of amphibian is this",
+//         imgSrc : "./assets/images/sonoran.jpg",
+//         choiceA : "Megophrys nasuta",
+//         choiceB : "Incilius alvarius",
+//         choiceC : "Dendrobates tinctorius",
+//         correct : "B"
+//     } , {
+//         question : "What species of mammal is this",
+//         imgSrc : "./assets/images/opossum.jpg",
+//         choiceA : "Didelphis virginiana",
+//         choiceB : "Procyon lotor",
+//         choiceC : "Pseudocheirus peregrinus",
+//         correct : "A"
+//     } , {
+//         question : "What species of fish is this?",
+//         imgSrc : "./assets/images/parrotfish.jpg",
+//         choiceA : "Cetoscarus ocellatus",
+//         choiceB : "Chlorurus michrohinos",
+//         choiceC : "Chlorurus sordidus",
+//         correct : "A"
+//     } ,
+//  ]
 
 //  create some variables
 
