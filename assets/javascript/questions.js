e487967689d4b45f6c5571669ac8497f2fc0899e
copiousamounts <nick.hernstedt@gmail.com>
2020-01-24 21:17:50 -0800

    changed some margins for timer

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,28 +1,28 @@
 //  create our questions
 let questions = [
   {
-      question : "What species of reptile is this",
+      question : "What species of reptile is this?",
       imgSrc : "./assets/images/coral.jpg",
       choiceA : "Nerodia rhombifer",
       choiceB : "Micrurus fulvius",
       choiceC : "Ophiophagus hannah",
       correct : "B"
   } ,   {
-     question : "What species of bird is this",
+     question : "What species of bird is this?",
      imgSrc : "./assets/images/harpy.jpg",
      choiceA : "Falco peregrinus",
      choiceB : "Haliaeetus leucocephalus",
      choiceC : "Harpia harpyja",
      correct : "C"
  } ,  {
-     question : "What species of amphibian is this",
+     question : "What species of amphibian is this?",
      imgSrc : "./assets/images/sonoran.jpg",
      choiceA : "Megophrys nasuta",
      choiceB : "Incilius alvarius",
      choiceC : "Dendrobates tinctorius",
      correct : "B"
  } , {
-     question : "What species of mammal is this",
+     question : "What species of mammal is this?",
      imgSrc : "./assets/images/opossum.jpg",
      choiceA : "Didelphis virginiana",
      choiceB : "Procyon lotor",
